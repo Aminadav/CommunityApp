8186c30852fedb38249d3eb512fdfaae42e08932
Tzafra <tzipporahwitty@gmail.com>
2016-12-19 22:31:28 +0200

    Embedded map

--- www/js/local/localController.js
+++ www/js/local/localController.js
@@ -11,6 +11,35 @@ angular.module('mijLocal')
                 map: map
             });
             $scope.map = map;
-        }
-        $scope.initializeMap();
+        },
+        $scope.getProperties = function() {
+            LocalService.getCompaniesProperties()
+                .then(function (data) {
+                    for (var i in data.data) {
+                        console.log(data.data[i].name)
+                    }
+                });
+        };
+
+        var companies = {};
+        $scope.getCompanies = function(offset){
+            LocalService.getCompanies(offset)
+                .then(function(data){
+                    var temp_companies = data.data['companies'];
+                    for(var i in temp_companies) {
+                        if(temp_companies[i].properties['address']) {
+                            console.log(temp_companies[i].properties['address'], temp_companies[i].properties['city']);
+                            companies[temp_companies[i].companyID] = temp_companies[i];
+                        }
+                    }
+                    console.log(companies);
+                    if(data.data['has-more']){
+                        $scope.getCompanies(data.data.offset);
+                    } else {
+                        console.log(companies);
+                    }
+                })
+        };
+
+        //$scope.initializeMap();
     })
--- www/js/local/localService.js
+++ www/js/local/localService.js
@@ -1,6 +1,14 @@
 angular.module('mijLocal')
-    .factory('LocalService', function(){
+    .factory('LocalService', function(HubspotService){
         var functions = {
+            getCompanies: function(offset){
+                if(!offset) var offset = 0;
+                var fields_string = '&properties=address&properties=address2&properties=city';
+                return HubspotService.get('/companies/v2/companies/paged?1=1'+fields_string, {offset: offset});
+            },
+            getCompaniesProperties: function(){
+                return HubspotService.get('/properties/v1/companies/properties/')
+            }
         };
         return functions;
     })
